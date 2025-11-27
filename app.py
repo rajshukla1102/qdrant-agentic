5e66f9d02169300a29902239ed1195e522391d8f
Rishabh Raj Shukla <rishabh.shukla@intelliconnectq.com>
2025-11-27 17:23:02 +0530

    Make the server port configurable via environment variable

--- app.py
+++ app.py
@@ -232,4 +232,5 @@ async def get_stats(x_ingest_token: Optional[str] = Header(None)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
